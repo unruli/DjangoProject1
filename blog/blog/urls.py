1b3791fdfd5261060a155fed69ada1bef7496306
Your-Full-Name <58475710+unruli@users.noreply.github.com>
2023-02-15 13:07:31 +0100

    new commit

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
-    path(' post/<int:pk>/delete/',BlogDeleteView.as_view(), name=' post_delete'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
